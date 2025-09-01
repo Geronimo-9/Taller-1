416d0da2e7a7916474eaa7cc047f22d4b40b945b
Geronimo-9 <gmijaresa@unicartagena.edu.co>
2025-09-01 16:12:50 -0500

    coreccion del ejercicio 1 taller 1

--- src/Libro.java
+++ src/Libro.java
@@ -1,15 +1,39 @@
-public abstract class Clases {
+public class Libro {
+    private String titulo;
+    private String autor;
+    private int numeroPaginas;
+
+
+     public Libro() {
+        this.titulo = "Meditaciones";
+        this.autor = "Aristoteles";
+        this.numeroPaginas = 250;
+    }
+
+
+
+    public Libro(String titulo, String autor, int numeroPaginas){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+
+    }
+
+
+
+
+
+
+    public String toString(){
+        return " Titulo: "+titulo+" Autor: "+autor+" Pag: "+numeroPaginas+" ";
+    }
+
+
+
 
 
 
 }
-
-
-
-
-
-}
-
 
 
 
--- src/Taller_1.java
+++ src/Taller_1.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
 
- public class Taller_1 {
-     public static void main(String[] args) {
+public class Taller_1 {
+    public static void main(String[] args) {
+
+//Scanner teclado = new Scanner(System.in);
+
+
+Libro l1 =  new Libro();
+        System.out.println(l1);
 
 
 
-
-
+Libro l2 = new Libro("Mi primera vez", "Guillermo", 200);
+        System.out.println(l2);
 
 
 
